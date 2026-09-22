decb1456f1299124f627b65089776c8663cb405c
ryleecutter <ryleecutter12345@live.com>
2026-09-22 19:09:35 -0400

    im sure ill have to debug, but this seems to be pretty good.

--- algorithms/GA.py
+++ algorithms/GA.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys 
 import random 
 from HC import randomTour, swap, cost
-import math
+from functools import partial
 
 #returns a list of pop_size number random traversals
 def getPopulation(matrix, pop_size, n):
@@ -27,20 +27,28 @@ def edgeMap(p1, p2, n):
     for j in range (n-1):
         if p2[j] in edgemap[p2[j+1]]:
             edgemap[p2[j+1]].remove(p2[j])
-            edgemap[p2[j+1]].add(-p2[j])
+            edgemap[p2[j+1]].add(-(p2[j]+1)) #holy testing cooked me. this so index 0 is fixed.
             edgemap[p2[j]].remove(p2[j+1])
-            edgemap[p2[j]].add(-p2[j+1])
+            edgemap[p2[j]].add(-(p2[j+1]+1))
         else:
             edgemap[p2[j]].add(p2[j+1])
             edgemap[p2[j+1]].add(p2[j])  
     
     return edgemap
 
+
+def decodeCity(c):
+    return abs(c) - 1 if c < 0 else c   
+
 #needs to go through every index in edge_map and remove current city. should be fast with set. O(n)
 def removeCity(edge_map, current_city):
+    
+    edge_map[current_city].clear() #we can quickly look and see its empty because we dont want to go to it since its already visited.
+    
     for x in edge_map:
         x.discard(current_city)
-        x.discard(-current_city)
+        x.discard(- (current_city+1))
+        
     
 
 
@@ -56,8 +64,13 @@ def parents(population, pop_size):
 #finds option with fewest edges, if tied chooses randomly.
 # refer to @fewestCurrent for within the set.
 def fewestEdges(edge_map, n, child):
-    fewest = [0]
-    for i in range(n):
+    available = [i for i in range(n) if i not in child]
+
+    if not available:
+        return -1
+
+    fewest = [available[0]]
+    for i in available[1:]:
         if len(edge_map[i]) == len(edge_map[fewest[0]]) and i not in child: #they are same amnt of edges so add to fewest list.
             fewest.append(i) 
         else: 
@@ -65,22 +78,39 @@ def fewestEdges(edge_map, n, child):
                 fewest = [i]
     return np.random.choice(fewest) if len(fewest) > 1 else fewest[0]
 
+
+
 #returns the city with the fewest connected edges in current_city's set.
 def fewestCurrent(edge_map, current_city):
     currset = edge_map[current_city]
-    if len(currset) == 0:
+
+    if not currset:
         return -1
-    priority_cities = [c for c in currset if c < 0]
-    
-    if priority_cities:
-        next_city = min(priority_cities, key=lambda c: len(edge_map[abs(c)])) #negative vlaues so only look for smallest edge with those. 
-    else:
-        next_city = min(currset, key=lambda c: len(edge_map[abs(c)])) #no priorties so we check these normally. and find smallest amnt of edges.        
-    return next_city
-        
+
+    candidates = [c for c in currset if c < 0]
+
+    if not candidates:
+        candidates = list(currset)
+
+    min_edges = min(len(edge_map[decodeCity(c)])
+                    for c in candidates
+                   )
+
+    tied = [c for c in candidates if len(edge_map[decodeCity(c)]) == min_edges]
+
+    return decodeCity(random.choice(tied)) 
+
+
+def mutation(path):
+    tpath = path.copy()
+    n = len(tpath)
+    validrange = range(1,n-1)
+    i,j = random.sample(validrange,2) #when we have enough nodes to have non trivial swaps.
+    tpath[i],tpath[j] = tpath[j],tpath[i]
+    return tpath #new child
 
 #returns each generation. so that main can run simAn on many gens. 
-def GA(matrix, n, pop_size, mut_chance, population):
+def GA(n, pop_size, mut_chance, population):
     
     p1,p2 = parents(population, pop_size) #get two parents
     edge_map = edgeMap(p1,p2, n) #if negative then its prioritized: common sequence in both parents
@@ -97,19 +127,29 @@ def GA(matrix, n, pop_size, mut_chance, population):
     #start the loop for the rest of traversal. 
     for i in range(1, n): #start at 1 because we already found child's first element. 
         current_city = fewestCurrent(edge_map, current_city)
-        if current_city == -1:
+        if current_city == -1: #then select a random city, as there were no edges.
             current_city = fewestEdges(edge_map, n, child)
         child.append(current_city) 
         removeCity(edge_map, current_city)
     child.append(child[0])
     
     #mutate child w/ prob mut_chance
-    ...
+    if random.uniform(0,1) < mut_chance:
+       return mutation(child)
+    
     return child
          
     
-def combineGenerations(population, children):
-    ...
+
+
+def combineGenerations(matrix,population, children):
+    #combine the two then sort them and use half the length 
+    #best fit is based on cost of traversal... 
+    costfunc = partial(cost, matrix=matrix)
+    sortedpop = sorted(population + children, key=costfunc) #has both lists sorted together.
+    return sortedpop[:len(sortedpop)//2]
+
+
 
 def main():
     
@@ -117,10 +157,10 @@ def main():
     n = matrix.shape[0] #amnt of row
     
     ##### HYPERPARAMETERS #####
-    mut_chance = 0 #the prob of mutating a child after creation
-    pop_size = 0  #the amount of possible parents for next generation
-    gen_num = 0 #how many full generations
-    children_gen = 0 #how many chilren per generation
+    mut_chance = .05 #the prob of mutating a child after creation
+    pop_size = 2  #the amount of possible parents for next generation > 1
+    gen_num = 3 #how many full generations
+    children_gen = 5 #how many chilren per generation
      ########################
     children = [] #list of children to be combined with pop after each gen. 
     
@@ -130,9 +170,10 @@ def main():
     for j in range(gen_num):#this many generations
         
         for i in range(children_gen): #make this many children per generation
-            children.append(GA(matrix, n, pop_size, mut_chance, population))
+            children.append(GA(n, pop_size, mut_chance, population))
+        #after each generation, combine the best fitting. 
+        population = combineGenerations(matrix, population, children)
         
-        population = combineGenerations(population, children)
     
 
 
