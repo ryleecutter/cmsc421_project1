5ccc5664d73d463528bbd5c5e845f62ab486cc4e
ryleecutter <ryleecutter12345@live.com>
2026-09-22 14:32:49 -0400

    did edgemap now need to do priority for both in edgemap

--- algorithms/GA.py
+++ algorithms/GA.py
@@ -8,9 +8,23 @@ import math
 def getPopulation(matrix, pop_size, n):
     return [randomTour(matrix, n)[1] for _ in range(pop_size)]
 
-
-def edgeMap(matrix, p1, p2):
-    ...
+#edgeMap will take in the two parents
+#returns a map where the key is a city and the value is a set with 
+#directly adjacent nodes in the parent traversals
+#should note that the edges which are in both should be prioritized, somehow. 
+#perhaps a value in the set which we check everytime 
+def edgeMap(matrix, p1, p2, n):
+    #need two structures;
+    #direct neighbor list from both
+    edgemap = [set()] * n  #list with set, same indices as given in matrix
+    for i in range(n-1):
+        edgemap[p1[i]].add(p1[i+1]) #add edges to edge map. set doesnt allow duplicates so safe.
+        edgemap[p1[i+1]].add(p1[i])
+        edgemap[p2[i]].add(p2[i+1])
+        edgemap[p2[i+1]].add(p2[i])
+    
+            
+    
 
 
 #returns each generation. so that main can run simAn on many gens. 
@@ -23,7 +37,7 @@ def simAn(matrix, n, pop_size, mut_chance, population):
         p2 = random.randint(0, pop_size - 1)
     
     #create the edgemap for the parents. 
-    edge_map = edgeMap(matrix, p1,p2)
+    edge_map = edgeMap(matrix, p1,p2, n)
 
 
 def main():
