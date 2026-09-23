a00521f3cfae7e350ca8e4500f4423516a2b59ec
ryleecutter <ryleecutter12345@live.com>
2026-09-23 17:57:04 -0400

    first iteration. probably terrible bugs lol

--- algorithms/AStar.py
+++ algorithms/AStar.py
@@ -6,42 +6,84 @@ from HC import cost
 from functools import partial
 from aima import utils
 
-#returns total weight of best mst
-def sumMST(sparse):
-    ...
 
-#returns the min dist from state to some city in unvisited.
-def minDist(matrix, unvisited, state):
-    ...
+#each state is represented by a node with: 
+#   current_state
+#   start_state
+#   g(n) cost
+#   #h(n) cost
+#   f(n) cost
+#   
+class State:
+    def __init__(self, current:int, visited, g, h, n : int, parent=None, start=None):
+        self.current = current
+        self.visited = visited
+        self.g = g
+        self.h = h
+        self.f = g + h
+        self.parent = parent
+        self.start = start
+        self.n = n
+    #define a heuristic cost. 
+    def heuristic(self, matrix):
+        if len(unvisited) == 0:
+            self.h = matrix[self.current][self.start]
+        else:
+            unvisited = self.unvisited()
+            mst_cost = self.sumMST(mst(matrix[np.ix_(unvisited, unvisited)]))
+            h1 = minDist(matrix, self.current, unvisited)
+            h2 = minDist(matrix, self.start, unvisited)
+            self.h = mst_cost + h1 + h2
+            self.f = self.g + self.h
+    #returns the sum of the best mst.
+    def sumMST(self, sparse):
+        return np.sum(sparse.toarray())
+    #inits the evaluation value
+    def fx(self):
+        self.f = self.g + self.h
+    def unvisited(self):
+       return set(range(self.n)) - self.visited
 
-def hn(matrix, state):
-    # h(n) = mst(unvisited cities) + mindist(unvisited, start) + mindist(current, unvisited)
-    # unvisited to start bcause it must connect at the end back to start. 
-    # current to unvisited to connect the current sequence to the mst. 
-    unvisited = state.unvisited #just storing cuz its used multiple times - lookup time
-    
-    return sumMST(mst(unvisited)) + minDist(matrix, unvisited, state.start) + minDist(matrix, unvisited, state.current)
+#returns min dist from a city to some city in unvisited
+def minDist(matrix, city, unvisited):
+    return min(matrix[city][x] for x in unvisited)
 
-def fn():
-    ...
-
-def memoize():
-    ...
+def path(matrix, state):
+    path = []
+    while state.parent != None:
+        path.append(state.current)
+        state = state.parent
+    path.append(state.current)
+    return path.reverse()
 
 def main():
     matrix = np.loadtxt(sys.argv[1])
     n = matrix.shape[0] # num rows
+        
     
-    #create visited
-    visited = set()
-    h = hn(matrix, state)#takes matrix and state which is from the fringe: contains all info we need. 
-    
-    f = fn()
-    f = memoize() #save msts so we dont have to recompute same states continuously. 
-    fringe = utils.PriorityQueue('min', ...) # need to define f. 
+    fringe = utils.PriorityQueue('min', f = lambda state: state.f) #sorts based on f 
 
-    #lets first create our heuristic function. 
-    
+    #need the initial cities.
+    for i in range(n):
+       x = State(current=i, visited={i}, g=0, h=0, n=n, parent=None, start=i)
+       x.heuristic(matrix)
+       x.fx()
+       fringe.append(x)
+
+    #pop from fringe
+    partialstate = fringe.pop    
+    #check goal
+    if len(partialstate.visited) == n:
+        return path(matrix, partialstate) #returns the path of the traversal
+    #generate successors
+    for x in partialstate.unvisited():
+         y = State(current=x, visited=partialstate.visited()+set(x), 
+                   g= partialstate.g + minDist(matrix,x,partialstate.unvisited() | {x}), 
+                   n=n, 
+                   parent=partialstate.current, start=partialstate.start)
+         y.heuristic(matrix)
+         y.fx()
+         fringe.append(y)
 
 
 
