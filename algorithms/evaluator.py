d60790ec7adfd14229783f9c46c58916329751dc
ryleecutter <ryleecutter12345@live.com>
2026-09-23 20:57:22 -0400

    all praise pdb step into ... so many dumb mistakes but it wasnt that bad after all.

---
+++ algorithms/evaluator.py
@@ -0,0 +1,20 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import time
+import sys
+
+
+
+
+
+
+def main():
+    algo = np.loadtxt(sys.argv[1]) # should go: python evaluator.py algo matrix
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
